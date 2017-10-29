1d2d67e7c6e74b467bae14c77795f88a3d81eb6c
iMac <iMac@LAPTOP-MJK7SBQ5>
2017-10-28 23:07:29 -0500

    Revert "reparacion registro persona"
    This reverts commit 7ddc148cdff89d13bed5eba9c2073101715cbbb5.

--- src/controcentrolintegradorproyectos/pantallas/bl/Login.java
+++ src/controcentrolintegradorproyectos/pantallas/bl/Login.java
@@ -3,8 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package controcentrolintegradorproyectos.pantallas;
+package controcentrolintegradorproyectos.pantallas.bl;
 
+import controcentrolintegradorproyectos.pantallas.*;
 import java.awt.Color;
 import java.awt.Cursor;
 import java.awt.Image;
